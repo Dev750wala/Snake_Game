ea8585e697340361d809d9e2961799e6dbb98aac
Dev750wala <117472132+Dev750wala@users.noreply.github.com>
2023-09-11 21:19:35 +0530

    scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -7,23 +7,30 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("yellow")
         self.hideturtle()
         self.speed("fastest")
         self.penup()
         self.goto(x=0, y=270)
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
 
     def update_score(self):
-        self.write(arg=f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        with open("high_score.txt") as high_score_text:
+            self.high_score = high_score_text.read()
+        self.write(arg=f"Score: {self.score} High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_is_over(self):
-        self.goto(0, 0)
-        self.color("White")
-        self.write(arg="GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+
+        with open("high_score.txt", mode='w') as update_high_score:
+            update_high_score.write(f"{self.high_score}")
+
+        self.score = 0
+        self.update_score()
 
     def count_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
 
